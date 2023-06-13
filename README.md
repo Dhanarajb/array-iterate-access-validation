89aea5406d210f9067686dfcdb15623f7f224fc7
Dhanarajb <88299676+Dhanarajb@users.noreply.github.com>
2023-06-14 00:00:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# array-iterate-access-validation
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-9xgmcw)
---
+++ script.js
@@ -0,0 +1,14 @@
+let data = [3, 2, 3, 4, 2, 25, 6, 7, 4, 3];
+
+// let newAr = [];
+// for (let i = 0; i < data.length; i++) {
+//   newAr.push(data[i]);
+// }
+// console.warn(newAr);
+
+// console.log(data[4])
+
+function getElement() {
+  let getData = document.getElementById('element').value;
+  console.log(data[getData]);
+}
